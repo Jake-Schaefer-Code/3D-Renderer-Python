5fe87b99fae8569f2cf5651eb178784b61edf1cb
Snakej07 <jacobrobertschaefer@gmail.com>
2024-02-19 00:48:58 -0600

    updates

--- constants.py
+++ constants.py
@@ -35,45 +35,35 @@ NEAR_PLANE_HEIGHT = 2 * math.tan(FOVY/2) * NEAR_Z
 NEAR_PLANE_WIDTH = 2 * math.tan(FOVY/2) * NEAR_Z * ASPECT_RATIO
 FOVX = math.atan((NEAR_PLANE_WIDTH/2)/NEAR_Z) * 2
 
-
-
-# define coordinate origin to be in center of the screen, input point is in this form
-SCREEN_ORIGIN = np.array([SCREEN_WIDTH/2, SCREEN_HEIGHT/2,0,1])
-SCREEN_SCALE = np.array([SCREEN_WIDTH/2, SCREEN_HEIGHT/2,1,0])
-
-
 # This is because we define the coordinate system such that +1 is up, but pygame defines it opposite
 PYGAME_WINDOW_SCALE = np.array([HALFWIDTH, -HALFHEIGHT]) 
 PYGAME_WINDOW_ORIGIN = np.array([HALFWIDTH, HALFHEIGHT])
 
-
-"""projM = create_projection_matrix([-NEAR_PLANE_WIDTH/2, -NEAR_PLANE_HEIGHT/2, NEAR_Z],[NEAR_PLANE_WIDTH/2, NEAR_PLANE_HEIGHT/2, FAR_Z])
-pn = projM @ np.array([-0.19101796, 0, 0.2,1])
-print(pn/pn[3])"""
-
-
-PLANES = np.array([np.array([-NEAR_Z, 0, NEAR_PLANE_WIDTH/2, 0]) , # right
+"""PLANES = np.array([np.array([-NEAR_Z, 0, NEAR_PLANE_WIDTH/2, 0]) , # right
                    np.array([NEAR_Z, 0, NEAR_PLANE_WIDTH/2, 0]) ,  # left
                    np.array([0, -NEAR_Z, NEAR_PLANE_HEIGHT/2, 0]) , # bottom
-                   np.array([0, NEAR_Z, NEAR_PLANE_HEIGHT/2, 0])]) # top
+                   np.array([0, NEAR_Z, NEAR_PLANE_HEIGHT/2, 0])]) # top"""
 
-# Normalizing plane normal vectors
-PLANE_NORMALS = np.array([plane/math.sqrt(plane[0]**2 + plane[1]**2 + plane[2]**2) for plane in PLANES])
-
-
-PLANES = np.array([np.array([-math.cos(FOVX/2), 0, math.sin(FOVX/2), 0]) , # right
+PLANES = np.empty((4,4))
+PLANES[0] = np.array([-math.cos(FOVX/2), 0, math.sin(FOVX/2), 0])
+PLANES[1] = np.array([math.cos(FOVX/2), 0, math.sin(FOVX/2), 0])
+PLANES[2] = np.array([0, -math.cos(FOVY/2), math.sin(FOVY/2), 0]) 
+PLANES[3] =  np.array([0, math.cos(FOVY/2), math.sin(FOVY/2), 0])
+"""PLANES = np.array([np.array([-math.cos(FOVX/2), 0, math.sin(FOVX/2), 0]) , # right
                    np.array([math.cos(FOVX/2), 0, math.sin(FOVX/2), 0]) ,  # left
                    np.array([0, -math.cos(FOVY/2), math.sin(FOVY/2), 0]) , # bottom
-                   np.array([0, math.cos(FOVY/2), math.sin(FOVY/2), 0])]) # top
+                   np.array([0, math.cos(FOVY/2), math.sin(FOVY/2), 0])]) # top"""
 
-
-PLANE_NORMALS = np.array([plane/math.sqrt(plane[0]**2 + plane[1]**2 + plane[2]**2) for plane in PLANES])
-
+# Normalizing plane normal vectors
+#PLANE_NORMALS = np.array([plane/math.sqrt(plane[0]**2 + plane[1]**2 + plane[2]**2) for plane in PLANES])
+PLANE_NORMALS = PLANES
 
 PLANE_POINTS = NEAR_Z * np.array([np.array([0, 0, 0, 0]), # right
                                   np.array([0, 0, 0, 0]),  # left
                                   np.array([0, 0, 0, 0]), # bottom
                                   np.array([0, 0, 0, 0])])
+
+
 
 BACKGROUND_COLOR = (80,85,90)
 SPEED = 0.025
