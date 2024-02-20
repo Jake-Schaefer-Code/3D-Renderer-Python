3c9ea3294d9d2237475364af2f062c2dfac4bcd2
Snakej07 <jacobrobertschaefer@gmail.com>
2024-02-19 22:02:06 -0600

    updates

--- constants.py
+++ constants.py
@@ -12,17 +12,19 @@ SCREEN_HEIGHT = 720
 HALFWIDTH = SCREEN_WIDTH/2
 HALFHEIGHT = SCREEN_HEIGHT/2
 
+# This is because we define the coordinate system such that +1 is up, but pygame defines it opposite
+PYGAME_WINDOW_SCALE = np.array([HALFWIDTH, -HALFHEIGHT]) 
+PYGAME_WINDOW_ORIGIN = np.array([HALFWIDTH, HALFHEIGHT])
+
 # Aspect ratio that converts y to x
 ASPECT_RATIO = SCREEN_WIDTH/SCREEN_HEIGHT
 
-
-
 # FOV in y
 FOVY = math.pi/2
-# Could be replaced with hitbox code later on
-NEAREST_VAL = 1
+
 # Distance to Near Plane -> closest value to see -> 0.1 = 10 cm
 NEAR_Z = 0.1
+
 # Distance to far plane -> furthest value to see -> 1 = 1 m
 FAR_Z = 10
 
@@ -32,27 +34,20 @@ FY = 1/math.tan(FOVY/2)
 # NEAR_Z / (WIDTH/2) -> x scaling factor
 FX = (1/math.tan(FOVY/2))/ASPECT_RATIO
 
+
 NEAR_PLANE_HEIGHT = 2 * math.tan(FOVY/2) * NEAR_Z
 NEAR_PLANE_WIDTH = 2 * math.tan(FOVY/2) * NEAR_Z * ASPECT_RATIO
+
+# FOV in x
 FOVX = math.atan((NEAR_PLANE_WIDTH/2)/NEAR_Z) * 2
 
-# This is because we define the coordinate system such that +1 is up, but pygame defines it opposite
-PYGAME_WINDOW_SCALE = np.array([HALFWIDTH, -HALFHEIGHT]) 
-PYGAME_WINDOW_ORIGIN = np.array([HALFWIDTH, HALFHEIGHT])
-
-"""PLANES = np.array([np.array([-NEAR_Z, 0, NEAR_PLANE_WIDTH/2, 0]) , # right
-                   np.array([NEAR_Z, 0, NEAR_PLANE_WIDTH/2, 0]) ,  # left
-                   np.array([0, -NEAR_Z, NEAR_PLANE_HEIGHT/2, 0]) , # bottom
-                   np.array([0, NEAR_Z, NEAR_PLANE_HEIGHT/2, 0])]) # top"""
-
+#### FRUSTUM ####
 PLANES = np.empty((4,4))
 PLANES[0] = np.array([-math.cos(FOVX/2), 0, math.sin(FOVX/2), 0]) # right
 PLANES[1] = np.array([math.cos(FOVX/2),  0, math.sin(FOVX/2), 0]) # left
 PLANES[2] = np.array([0, -math.cos(FOVY/2), math.sin(FOVY/2), 0]) # bottom
 PLANES[3] = np.array([0, math.cos(FOVY/2),  math.sin(FOVY/2), 0]) # top
 
-# Normalizing plane normal vectors
-#PLANE_NORMALS = np.array([plane/math.sqrt(plane[0]**2 + plane[1]**2 + plane[2]**2) for plane in PLANES])
 PLANE_NORMALS = PLANES
 
 PLANE_POINTS = NEAR_Z * np.array([np.array([0, 0, 0, 0]), # right
@@ -65,3 +60,13 @@ PLANE_POINTS = NEAR_Z * np.array([np.array([0, 0, 0, 0]), # right
 BACKGROUND_COLOR = (80,85,90)
 SPEED = 0.025
 MAX_FPS = 60
+ROT_ANGLE = math.pi/360
+RATE = SPEED * MAX_FPS
+
+"""PLANES = np.array([np.array([-NEAR_Z, 0, NEAR_PLANE_WIDTH/2, 0]) , # right
+                   np.array([NEAR_Z, 0, NEAR_PLANE_WIDTH/2, 0]) ,  # left
+                   np.array([0, -NEAR_Z, NEAR_PLANE_HEIGHT/2, 0]) , # bottom
+                   np.array([0, NEAR_Z, NEAR_PLANE_HEIGHT/2, 0])]) # top"""
+
+# Normalizing plane normal vectors
+#PLANE_NORMALS = np.array([plane/math.sqrt(plane[0]**2 + plane[1]**2 + plane[2]**2) for plane in PLANES])
